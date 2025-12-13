c8c210800038714e06385765ad3ed36396a25986
Danielle Heberling <deeheber@users.noreply.github.com>
2025-12-13 10:58:09 -0800

    Simplify and refactor code

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -22,7 +22,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
     <li className="my-6">
       <a
         href={href}
-        className="text-skin-accent inline-block text-lg font-medium decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="external-link"
       >
         {secHeading ? (
           <h2 {...headerProps}>{title}</h2>
